689a07c8d4530bad51d0286b5b15fe720107c180
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-05-13 19:16:59 +0200

    Add dropdown icon

--- assets/components/headerComponent.vue
+++ assets/components/headerComponent.vue
@@ -7,11 +7,17 @@
         <nav class="navMenu">
             <NuxtLink to="/">Inicio</NuxtLink>
             <NuxtLink to="/sobre-nosotros">Sobre Nosotros</NuxtLink>
-            <NuxtLink to="/">Carta y Menús</NuxtLink>
+            <NuxtLink to="/" class="dropdown">Carta y Menús <svg class="navMenuIcon" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 15 15"><path fill="currentColor" d="M7.5 10.207L11.707 6H3.293z"/></svg></NuxtLink>
             <NuxtLink to="/">Contacto y Horario</NuxtLink>
         </nav>    
     </header>
 </template>
+
+<script>
+    export default {
+        name: 'TeenyiconsDownSmallSolid'
+    }
+</script>
 
 <style scoped>
     @import "/assets/css/main/header.css";
