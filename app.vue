cbc86407e884c999ebcc8d8bc2f663d1b309ab37
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-05-13 18:30:40 +0200

    Create home page and about us page. Also, nav created

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <headerComponent />
+  <div class="bodyContainer">
+    <NuxtPage />
   </div>
 </template>
+
+<script setup>
+  import headerComponent from './assets/components/headerComponent.vue'
+</script>
+
+<style scoped>
+  @import "/assets/css/main/main.css";
+</style>
---
+++ assets/components/headerComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li><NuxtLink to="/">Inicio</NuxtLink></li>
+                <li><NuxtLink to="/sobre-nosotros">Sobre Nosotros</NuxtLink></li>
+                <li><NuxtLink to="/">Carta y menús</NuxtLink></li>
+                <li><NuxtLink to="/">Contacto y Horario</NuxtLink></li>
+            </ul>
+        </nav>    
+    </header>
+</template>
+
+<style scoped>
+    @import "/assets/css/main/header.css";
+</style>
