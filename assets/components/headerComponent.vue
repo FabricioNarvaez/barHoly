f30430a018396639b7b7211d34a5857d2064f614
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-05-17 17:33:52 +0200

    Add toggleDropdown to show and hide

--- assets/components/headerComponent.vue
+++ assets/components/headerComponent.vue
@@ -7,9 +7,9 @@
         <nav class="navMenu">
             <NuxtLink class="navMenuItem" title="Inicio | Bar Restaurante Holy" to="/">Inicio</NuxtLink>
             <NuxtLink class="navMenuItem" title="Sobre Nosotros | Bar Restaurante Holy" to="/sobre-nosotros">Sobre Nosotros</NuxtLink>
-            <div>
+            <div @click="toggleDropdown">
                 <span class="navMenuItem dropdown">Carta y Menús <svg class="navMenuIcon" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 15 15"><path fill="currentColor" d="M7.5 10.207L11.707 6H3.293z"/></svg></span>
-                <div class="dropdownMenu">
+                <div class="dropdownMenu" :class="{ dropdownActive: isDropdownActive}" >
                     <NuxtLink class="navMenuItem" title="Menú | Bar Restaurante Holy" to="/menu">Menú</NuxtLink>
                     <NuxtLink class="navMenuItem" title="Nuestros Pinchos | Bar Restaurante Holy" to="/nuestros-pinchos">Nuestros Pinchos</NuxtLink>
                     <NuxtLink class="navMenuItem" title="Bebidas | Bar Restaurante Holy" to="/bebidas">Bebidas</NuxtLink>
@@ -21,10 +21,13 @@
     </header>
 </template>
 
-<script>
-    // export default {
-    //     name: 'TeenyiconsDownSmallSolid'
-    // }
+<script setup>
+    import { ref } from "vue";
+
+    const isDropdownActive = ref(false);
+    const toggleDropdown = () => {
+        isDropdownActive.value = !isDropdownActive.value;
+    }
 </script>
 
 <style scoped>
