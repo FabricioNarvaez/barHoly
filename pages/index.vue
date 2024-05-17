a329c7ef661c230cef1840e819348853a13de4ce
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-05-17 20:04:05 +0200

    Add index page skeleton

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,15 @@
 <template>
-    <div><h1>Bar Holy</h1></div>
+    <div class="homeContainer">
+        <p>carrusel</p>
+        <div><h1>¡Bienvenido a Bar Restaurante Holy!</h1></div>
+        <p>En un mundo lleno de prisas y estrés, es necesario tener un lugar donde podamos relajarnos y disfrutar de una buena comida en un ambiente acogedor y divertido. Y ese lugar es sin duda el Bar Restaurant Holy.</p>
+        <img src="https://res.cloudinary.com/dzd68sxue/image/upload/v1715968749/Bar%20Holy/tapa_lgm5dt.webp"/>
+    </div>
 </template>
 
 <script setup>
 </script>
 
 <style scoped>
+    @import "/assets/css/home/home.css";
 </style>
