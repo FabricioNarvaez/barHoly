e526ca589ceecf6aae6a2d2079a4bcfd94cfd9d1
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-05-17 17:05:53 +0200

    Add new styles for header nav

--- assets/components/headerComponent.vue
+++ assets/components/headerComponent.vue
@@ -5,18 +5,26 @@
                 <span>Bar Restaurante Holy</span>
         </NuxtLink>
         <nav class="navMenu">
-            <NuxtLink to="/">Inicio</NuxtLink>
-            <NuxtLink to="/sobre-nosotros">Sobre Nosotros</NuxtLink>
-            <NuxtLink to="/" class="dropdown">Carta y Menús <svg class="navMenuIcon" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 15 15"><path fill="currentColor" d="M7.5 10.207L11.707 6H3.293z"/></svg></NuxtLink>
-            <NuxtLink to="/">Contacto y Horario</NuxtLink>
+            <NuxtLink class="navMenuItem" title="Inicio | Bar Restaurante Holy" to="/">Inicio</NuxtLink>
+            <NuxtLink class="navMenuItem" title="Sobre Nosotros | Bar Restaurante Holy" to="/sobre-nosotros">Sobre Nosotros</NuxtLink>
+            <div>
+                <span class="navMenuItem dropdown">Carta y Menús <svg class="navMenuIcon" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 15 15"><path fill="currentColor" d="M7.5 10.207L11.707 6H3.293z"/></svg></span>
+                <div class="dropdownMenu">
+                    <NuxtLink class="navMenuItem" title="Menú | Bar Restaurante Holy" to="/menu">Menú</NuxtLink>
+                    <NuxtLink class="navMenuItem" title="Nuestros Pinchos | Bar Restaurante Holy" to="/nuestros-pinchos">Nuestros Pinchos</NuxtLink>
+                    <NuxtLink class="navMenuItem" title="Bebidas | Bar Restaurante Holy" to="/bebidas">Bebidas</NuxtLink>
+                    <NuxtLink class="navMenuItem" title="Vinos | Bar Restaurante Holy" to="/vinos">Vinos</NuxtLink>
+                </div>
+            </div>
+            <NuxtLink class="navMenuItem" title="Contacto | Bar Restaurante Holy" to="/contacto">Contacto y Horario</NuxtLink>
         </nav>    
     </header>
 </template>
 
 <script>
-    export default {
-        name: 'TeenyiconsDownSmallSolid'
-    }
+    // export default {
+    //     name: 'TeenyiconsDownSmallSolid'
+    // }
 </script>
 
 <style scoped>
