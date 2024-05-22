ddd437054198de8ea37df4f2b871fa52d5a3eb93
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-05-22 21:15:07 +0200

    Add about us page and add shadow and padding to home image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,9 +12,10 @@ export default defineNuxtConfig({
       meta: [
         { name: 'description', content: 'Descubre nuestro exquisito menú, ambiente acogedor y servicio excepcional. ¡Haz de cada visita una ocasión memorable en el corazón de la ciudad!'},
         { name: 'robots', content: 'index, follow'},
-        { name: 'author', content: 'Fabricio Narváez'},
+        { name: 'author', content: 'Fabricio Narváez'}
       ],
       link: [
+        { rel: 'canonical', href: "https://www.holybarrestaurante.com/"},
         { rel: 'preconnect', href: 'https://fonts.googleapis.com'},
         { rel: 'preconnect', href: 'https://fonts.gstatic.com'},
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Bebas+Neue&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Raleway:ital,wght@0,100..900;1,100..900&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap'}
--- pages/sobre-nosotros.vue
+++ pages/sobre-nosotros.vue
@@ -1,12 +1,24 @@
 <template>
-    <h1>about</h1>
+  <div class="aboutUsContainer">
+    <h1>Sobre Nosotros</h1>
+    <section class="aboutUsDescription">
+      <p>En <strong>Bar Restaurant Holy</strong>  nos esforzamos por ofrecer a nuestros clientes una <strong>experiencia única </strong> y <strong>llena de sabor</strong>. Nuestro menú está compuesto por una deliciosa fusión de sabores, con platos que van desde lo tradicional hasta lo más innovador. Además, nuestro ambiente es <strong>divertido</strong> y <strong>acogedor</strong>, perfecto para disfrutar de una cena con amigos o para una noche de celebración.</p>
+      <p>Ven a conocernos y descubre por qué somos el lugar ideal para disfrutar de buena comida, buenos tragos y buenos momentos.</p>
+      <p class="aboutUsSeeYouSoon">¡Te esperamos en Bar Restaurant Holy!</p>
+    </section>
+  </div>
 </template>
 
 <script setup>
     useHead({
-      title: 'Sobre Nosotros | Bar Holy',
+      title: 'Sobre Nosotros | Bar Restaurante Holy',
       meta: [
-        { name: 'description', content: 'Sobre nosotros'}
+        { name: 'description', content: 'En Bar Restaurante Holy, creemos en el poder de los ingredientes frescos y de origen local para crear obras maestras culinarias. Experimente nuestra dedicación a la calidad en cada plato y bebida.'},
+        { rel: 'canonical', href: "https://www.holybarrestaurante.com/sobre-nosotros"},
       ]
     })
 </script>
+
+<style scoped>
+  @import "../assets/css/about-us/about-us.css"
+</style>
