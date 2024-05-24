f70d895cdcc3ed9d67150ea3fb6884a190a53801
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-05-24 17:39:26 +0200

    Add políticas de privacidad page

---
+++ pages/politicas-de-privacidad.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="legalTermsContainer">
+        <h1>Aviso Legal</h1>
+    
+        <h2>1. Objeto</h2>
+        <p>La presente Política de Privacidad (en adelante, la Política) regula el tratamiento de los datos personales de los usuarios del sitio web <strong>https://barrestauranteholy.es</strong> (en adelante, el Sitio Web).</p>
+
+        <h2>2. Cookies</h2>
+        <p>El Sitio Web puede utilizar cookies con el único fin de analizar el tráfico del sitio web.</p>
+    
+        <h2>3. Enlaces a otros sitios web</h2>
+        <p>El Sitio Web puede contener enlaces a otros sitios web de terceros. El Propietario no se responsabiliza del tratamiento de los datos personales que los usuarios puedan realizar en dichos sitios web.</p>
+    
+        <h2>4. El Sitio Web no almacena datos personales</h2>
+        <p>Es importante destacar que el Sitio Web no almacena ningún tipo de dato personal de los usuarios. La información que se recopila a través del Sitio Web, como la dirección IP o los datos de navegación, se utiliza únicamente para fines estadísticos y no se asocia a ninguna persona identificable.</p>
+    
+        <h2>5. Contacto</h2>
+        <p>Este sitio web dispone de información de contacto que permite a los usuarios realizar cualquier tipo de consulta. Para cualquier duda o aclaración sobre la presente Política, el usuario puede ponerse en contacto con el Propietario a través de la siguiente dirección de correo electrónico: <strong>barholypamplona006@gmail.com</strong>.</p>
+        
+        <h2>6. Modificaciones de la Política</h2>
+        <p>El Propietario se reserva el derecho a modificar la presente Política para adaptarla a las novedades legislativas o jurisprudenciales, así como a sus prácticas internas. Las modificaciones entrarán en vigor a partir de su publicación en el Sitio Web.</p>
+        
+        <h2>7. Aceptación de la Política</h2>
+        <p>El usuario declara haber leído, entendido y aceptado todas las condiciones de la presente Política de Privacidad.</p>
+    </div>
+</template>
+
+<style scoped>
+    @import "../assets/css/legalTerms/legalTerms.css";
+</style>
+
