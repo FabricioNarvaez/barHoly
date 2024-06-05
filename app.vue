bfa2331e91907633f141e95661894554fbd2fb12
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-06-05 19:29:16 +0200

    Add topButton

--- app.vue
+++ app.vue
@@ -1,16 +1,31 @@
 <template>
   <headerComponent />
-  <!-- <div class="bodyContainer"> -->
-    <NuxtPage />
-  <!-- </div> -->
+  <NuxtPage />
+  <button v-if="isVisible" id="topButton" @click="scrollToTop">
+    <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 40 40">
+      <path fill="currentColor" d="M4.659 28.167h30.682L20 7.833z" />
+    </svg>
+  </button>
   <footerComponent />
 </template>
 
 <script setup>
+  import { ref, onMounted } from 'vue';
   import headerComponent from './assets/components/headerComponent.vue';
   import footerComponent from './assets/components/footerComponent.vue';
-</script>
 
-<style scoped>
-  /* @import "/assets/css/main/main.css"; */
-</style>
+  const isVisible = ref(false);
+  onMounted(() => {
+    const handleScroll = () => {
+      isVisible.value = window.scrollY > 40;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  });
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+</script>
