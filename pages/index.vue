58416f137af79acd7b5d6edf5dbee4e44bc3dd35
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-08-03 12:33:14 +0200

    Fix restaurant name

--- pages/index.vue
+++ pages/index.vue
@@ -26,10 +26,10 @@
             <div class="homeServices">
                 <h2 class="subtitle">Sobre Nosotros</h2>
                 <section class="homeSection">
-                    <p>En <strong>Bar Restaurant Holy</strong>  nos esforzamos por ofrecer a nuestros clientes una <strong>experiencia única </strong> y <strong>llena de sabor</strong>. Nuestro menú está compuesto por una deliciosa fusión de sabores, con platos que van desde lo tradicional hasta lo más innovador.</p>
+                    <p>En <strong>Bar Restaurante Holy</strong>  nos esforzamos por ofrecer a nuestros clientes una <strong>experiencia única </strong> y <strong>llena de sabor</strong>. Nuestro menú está compuesto por una deliciosa fusión de sabores, con platos que van desde lo tradicional hasta lo más innovador.</p>
                     <p>Además, nuestro ambiente es <strong>divertido</strong> y <strong>acogedor</strong>, perfecto para disfrutar de una cena con amigos o para una noche de celebración.</p>
                     <p>Ven a conocernos y descubre por qué somos el lugar ideal para disfrutar de buena comida, buenos tragos y buenos momentos.</p>
-                    <p class="aboutUsSeeYouSoon">¡Te esperamos en Bar Restaurant Holy!</p>
+                    <p class="aboutUsSeeYouSoon">¡Te esperamos en Bar Restaurante Holy!</p>
                 </section>
             </div>
             <NuxtLink class="button" title="Contacto" to="/contacto">Contáctanos</NuxtLink>
